61be4713aa20c611a51d738fe0b5b9f51b2bb3d3
Nathan Zheng <nyz3@cornell.edu>
2018-07-03 16:40:22 +0800

    converted project into web project

--- ImageRecognitionAPI_gogs/src/OCRServlet/GoogleOCRHttpServlet.java
+++ ImageRecognitionAPI_gogs/src/OCRServlet/GoogleOCRHttpServlet.java
@@ -12,30 +12,6 @@ import org.json.JSONObject;
 public class GoogleOCRHttpServlet extends HttpServlet {
 	
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		
-		String requestUrl = request.getRequestURI();
-		String name = requestUrl.substring("/people/".length());
-		
-		Person person = DataStore.getInstance().getPerson(name);
-		
-		if(person != null){
-			String json = "{\n";
-			json += "\"name\": " + JSONObject.quote(person.getName()) + ",\n";
-			json += "\"about\": " + JSONObject.quote(person.getAbout()) + ",\n";
-			json += "\"birthYear\": " + person.getBirthYear() + "\n";
-			json += "}";
-			response.getOutputStream().println(json);
-		}
-		else{
-			//That person wasn't found, so return an empty JSON object. We could also return an error.
-			response.getOutputStream().println("{}");
-		}
-	}
-	
-	
-
-	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 		String name = request.getParameter("name");
