83a9fc67ad3a2d070508551878670418f4c6b768
Nathan Zheng <nyz3@cornell.edu>
2018-07-13 19:59:19 +0800

    refactored logic for image processing OCR Baidu, also added compression functionality

--- nathan_shuke/src/main/java/ImageRecognitionAPIs/ImageProcessingBaiduOCR.java
+++ nathan_shuke/src/main/java/ImageRecognitionAPIs/ImageProcessingBaiduOCR.java
@@ -1,11 +1,17 @@
 package main.java.ImageRecognitionAPIs;
 
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,6 +26,8 @@ public class ImageProcessingBaiduOCR {
 	
 	private static byte[] imageBytes;	//Byte Array representation of image to process.
 	private static String imageBase64;	//Base-64 String that represents encoding of image to process.
+	private static String filetype; //.png, .jpg, or .bmp, etc.
+	private static int byteSize; //size of image in bytes
 	
 	/**
 	 * Note - Image is received as a MultipartFile Object, sent for API results as Base-64 encoded string.
@@ -40,52 +48,107 @@ public class ImageProcessingBaiduOCR {
 	 * @param image
 	 * 	- the image file to inspect
 	 * @return
-	 * 	- whether or not the image is properly formatted and ready to be sent for API request.
+	 * 	- 0 if format incorrect, 1 if needs compression, 2 if format correct
 	 */
-	public static boolean checkFormat(MultipartFile image) {
+	public static int checkFormat(MultipartFile image) {
 		
 		try {
 			imageBytes = image.getBytes();
 			imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
 		} catch (Exception e) {
 			System.out.println("MultipartFile could not be encoded as Base-64 string.");
-			return false;
+			return 0;
 		}
+		
+		//compressible = true (image needs compression but other formatting correct), compression = false (no compression)
+		boolean compressible = false;
+		
+		if(!properType()) {
+			return 0;
+		} 
 		
 		if(!properSize()) {
-			return false;
-		} else if(!properType()) {
-			return false;
-		} else if(!properDimensions()) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	/**
-	 * 2. Find the size of the photo to prevent excessive uploads (max. 4 megabytes).
-	 * @return
-	 *     - True if file is under 4 MB., false otherwise.
-	 */
-	public static boolean properSize() {
+			compressible = true;
+		} 
 		
-		double numBytes = imageBytes.length;
-		if((numBytes / 1048576) > 4) {
-			System.out.println("File size exceeds 4 megabytes (MB), please reduce size.");
-			
-			//COMPRESS IMAGE
-			
-			
-			
-			return false;
+		if(!properDimensions()) {
+			return 0;
 		} else {
-			return true;
+			if(compressible) {
+				return 1;
+			} else {
+				return 2;
+			}
 		}
 	}
 	
 	/**
-	 * 3. Ensure image file is .jpg, .png, or .bmp
+	 * Compresses MultipartFile image if exceeds 4MB limit, called after other formatting
+	 * properties are verified to be correct.
+	 * @param image
+	 * @return byte[] holding compressed image information.
+	 */
+	public static byte[] compressImage(MultipartFile image) {
+		
+		try {
+			imageBytes = image.getBytes();
+			imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
+		} catch (Exception e) {
+			System.out.println("MultipartFile could not be encoded as Base-64 string.");
+			return null;
+		}
+		
+		//Finds a ImageWriter that can process the type of image (filetype) inputted (.jpg, .png, .bmp)
+		ImageWriter jpgWriter = ImageIO.getImageWritersByFormatName(filetype).next();
+		
+		//Get the default configuration parameters of the ImageWriter
+		ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam();
+		
+		//Allow for Compression Parameters to be explicitly stated/modified
+		jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		
+		//Compression quality expressed as a float (currently compress to 70%)
+		jpgWriteParam.setCompressionQuality(0.7f);
+		
+		//read compressed image data into compressedBytes ByteArray stream
+		ByteArrayOutputStream compressedByteStream = new ByteArrayOutputStream();
+		
+		//create output stream for compressed image bytes
+		ImageOutputStream outputStream = null;
+		try {
+			outputStream = ImageIO.createImageOutputStream(compressedByteStream);
+		} catch (IOException e) {
+			System.out.println("Output stream for compressed image bytes could not be initialized");
+			return null;
+		} 
+		jpgWriter.setOutput(outputStream);
+		
+		//convert original image data into bufferedImage to allow for compression
+		BufferedImage originalImage = null;
+		try {
+			originalImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+		} catch (IOException e) {
+			System.out.println("Original image could not be read into a byte array input stream.");
+		}
+		
+		//writes compressed image data into outputStream (ImageOutputStream)
+		IIOImage outputImage = new IIOImage(originalImage, null, null);
+		try {
+			jpgWriter.write(null, outputImage, jpgWriteParam);
+		} catch (IOException e) {
+			System.out.println("Compressed image data could not written into ImageOutputStream.");
+		}
+		jpgWriter.dispose();
+		
+	//	return compressed byte array
+		byte[] compressedImageBytes = null;
+		
+		return compressedImageBytes;
+		
+	}
+	
+	/**
+	 * 2. Ensure image file is .jpg, .png, or .bmp
 	 * @param inputEncoding
 	 *     - String to check for proper file extension (.jpg, .png, or .bmp).
 	 * @return
@@ -100,17 +163,39 @@ public class ImageProcessingBaiduOCR {
 		String prefixPNG = hexString.substring(0,16);
 		
 		if(prefixJPG.equals("FFD8FFDB")) {		//.jpg, .jpeg
+			filetype = "jpg";
 			return true;
 		} else if(prefixJPG.equals("FFD8FFE0") && prefixJPGTwo.equals("4A4649460001")) {
+			filetype = "jpg";
 			return true;
 		} else if(prefixJPG.equals("FFD8FFE1") && prefixJPGTwo.equals("457869660000")) {
+			filetype = "jpg";
 			return true;
-		} else if(prefixPNG.equals("89504E470D0A1A0A")) {	 //.png				
+		} else if(prefixPNG.equals("89504E470D0A1A0A")) {	 //.png		
+			filetype = "png";
 			return true;
 		} else if(prefixBMP.equals("424D")) {	//.bmp
+			filetype = "bmp";
 			return true;
 		} else {
 			return false;
+		}
+	}
+	
+	
+	/**
+	 * 3. Find the size of the photo to prevent excessive uploads (max. 4 megabytes).
+	 * @return
+	 *     - True if file is under 4 MB., false otherwise.
+	 */
+	public static boolean properSize() {
+		
+		byteSize = imageBytes.length;
+		if((byteSize / 1048576) > 4) {
+			System.out.println("File size exceeds 4 megabytes (MB), please reduce size.");
+			return false;
+		} else {
+			return true;
 		}
 	}
 	
