8370ae4ffef065048a5a32e697ccfd9fd4ad7001
Nathan Zheng <nyz3@cornell.edu>
2018-07-13 15:56:48 +0800

    created basic JDBC infrastructure for productheadersAPI

--- nathan_shuke/src/APISQLProductRetrieval/ProductHeadersAPI.java
+++ nathan_shuke/src/APISQLProductRetrieval/ProductHeadersAPI.java
@@ -9,6 +9,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import com.mysql.jdbc.Driver;
+
 /**
  * This class represents an API dedicated towards retrieving product category headers from the SQLServer and presenting data
  * for the front-end for mobile display purposes.
@@ -24,7 +33,32 @@ public class ProductHeadersAPI {
 	@Produces(MediaType.APPLICATION_JSON)  //returns query result in JSON format
 	public String textDetect() throws Exception {
 		
-		return null;
+		Connection connect = null;
+//	    Statement statement = null;
+	    PreparedStatement preparedStatement = null; //preparedStatement allows for dynamic SQLQuery creation
+	    ResultSet resultSet = null;
+	    
+	    String resultJSON = null;    //result from SQLQuery in JSON format
+	    
+	    try {
+            // This will load the MySQL driver, each DB has its own driver
+	    	new Driver();
+            // Setup the connection with the DB
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw");
+            // Statements allow to issue SQL queries to the database
+         //   statement = connect.createStatement();
+            // Result set get the result of the SQL query
+         //   resultSet = statement.executeQuery("select * from feedback.comments");
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            resultSet.close();
+            preparedStatement.close();
+            connect.close();
+        }
+	    
+		return resultJSON;
 		
 	}
 }
