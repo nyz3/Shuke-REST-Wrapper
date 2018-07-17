9d449d6eeaf594546332fe1fb3259c255098874c
Nathan Zheng <nyz3@cornell.edu>
2018-07-17 16:48:34 +0800

    wrote productretrieval method

--- nathan_shuke/src/main/java/SQLProductRetrievalAPIs/ProductInformationSQLRetrieval.java
+++ nathan_shuke/src/main/java/SQLProductRetrievalAPIs/ProductInformationSQLRetrieval.java
@@ -1,13 +1,11 @@
 package main.java.SQLProductRetrievalAPIs;
 
+import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-import com.mysql.jdbc.Driver;
+import com.mysql.cj.jdbc.Driver;
 
 /**
  * This class is dedicated towards retrieving product category headers and product information/details from the SQLServer and presenting data
@@ -18,71 +16,82 @@ import com.mysql.jdbc.Driver;
  */
 
 public class ProductInformationSQLRetrieval {
-
-	public String productHeaders() throws Exception {
+	
+	/**
+	 * This method returns results of a JDBC SQLQuery that represents all the product categories.
+	 * 
+	 * @return results - ArrayList<String> containing all results from query in a custom-organized fashion.
+	 * @throws Exception
+	 */
+	public static ArrayList<String> productHeaders() throws Exception {
 		
-		Connection conn = null;
-		String url = "jdbc:mysql://localhost:3306/db_name";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "root";
-        String password = "";
-        try {
-        Class.forName(driver);
-        conn = DriverManager.getConnection(url,userName,password);
-        System.out.print("Connection estd");
-        Statement st = conn.createStatement();
-        st.executeQuery("insert into table_name values(2, 'testing');");
-        } catch (Exception e) {
-            System.out.print("Error : " +e.getMessage());
-        } finally {
-        	
-        }
-        
-        // break 
-        
-        
 		Connection connect = null;
-//	    Statement statement = null;
-	    PreparedStatement preparedStatement = null; //preparedStatement allows for dynamic SQLQuery creation
-	    ResultSet resultSet = null;
-	    
-	    
-	    try {
-            // This will load the MySQL driver, each DB has its own driver
-	    	new Driver();
-            // Setup the connection with the DB
-	    	connect = DriverManager.getConnection("jdbc:mysql://60.205.209.93/feedback?" + "user=kc&password=kc");
-
-         //   connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw");
-            // Statements allow to issue SQL queries to the database
-         //   statement = connect.createStatement();
-            // Result set get the result of the SQL query
-         //   resultSet = statement.executeQuery("select * from feedback.comments");
-
-        } catch (Exception e) {
-        	
-            throw e;
-            
-        } finally {
-        	
-            resultSet.close();
-            preparedStatement.close();
-            connect.close();
-            
-        }
-	    
+		//address of the SQL database containing all the tables, database access username, password.
+		String databaseURL = "jdbc:mysql://60.205.209.93/kc?useSSL=false";  
+        String username = "kc";
+        String password = "kc";
+        
+        //query contains the SQLquery to be executed, PreparedStatement protects against SQL injection attacks
+        //by automatically escaping special SQL keywords.
+        PreparedStatement query = null;
+        String queryText = "SELECT * FROM tb_goods_class"; //QUERY COMMAND GOES HERE
+        ResultSet resultSet = null;
+        ArrayList<String> results = new ArrayList<String>(); //results to be organized from query for return
+        
+        //Execute query and fetch/return results.
+        try {
+	        	// This will load the MySQL driver, each DB has its own driver class instance
+		        new Driver();
+		        // Setup the connection with the DB
+		        connect = DriverManager.getConnection(databaseURL, username, password);
+		        //TESTING
+		        System.out.println("Connection established.");
+		        //prepare query command to be executed over the connection to database
+		        query = connect.prepareStatement(queryText);
+		        //execute query, and store query results in resultSet
+		        resultSet = query.executeQuery();
+		        //PROCESS resultSet and return data in a easily accessible and organized manner for display.
+		        int rowIndex = 1;	//index to parse all columns of resultSet, starts from 1 not 0
+		        //parses through the resultSet and prints out the String representation of each datapiece column by column
+		        //ResultSet.next iterates by row. Call get(index) to access items in each row.
+		        while (resultSet.next()) {
+		        	//iterate each row by column.
+		        	for(int colIndex = 1; colIndex < resultSet.getMetaData().getColumnCount(); colIndex++) {
+		        		results.add(resultSet.getString(colIndex));
+		        	}
+		            rowIndex++;
+		        }
+        	} catch (Exception e) {
+        		System.out.print("Error : " + e.getMessage());
+        	} finally {
+        		//close all connections and streams
+        		resultSet.close();
+                query.close();
+                connect.close();
+        	}
+        
+        return results;
+	}
+        
+	/**
+	 * This method returns results of a JDBC SQLQuery that represents all the product categories.
+	 * 
+	 * @return results - ArrayList<String> containing all results from query in a custom-organized fashion.
+	 * @throws Exception
+	 */
+	public static String getCategoryProducts(String category) throws Exception {
+		
 		return null;
 		
 	}
-
 	
-	public String getCategoryProducts(String category) throws Exception {
-		
-		return null;
-		
-	}
-	
-	public String getProductInfo(String product) throws Exception {
+	/**
+	 * This method returns results of a JDBC SQLQuery that represents all the product categories.
+	 * 
+	 * @return results - ArrayList<String> containing all results from query in a custom-organized fashion.
+	 * @throws Exception
+	 */
+	public static String getProductInfo(String product) throws Exception {
 		
 		return null;
 		
--- nathan_shuke/src/main/java/SQLProductRetrievalAPIs/TestingSQLAPI.java
+++ nathan_shuke/src/main/java/SQLProductRetrievalAPIs/TestingSQLAPI.java
@@ -1,5 +1,8 @@
 package main.java.SQLProductRetrievalAPIs;
 
+import java.util.ArrayList;
+
+import main.java.SQLProductRetrievalAPIs.ProductInformationSQLRetrieval;
 /**
  * Temporary class used to test effectiveness/accuracy of Product APIs that retrieve data from SQL database.
  * 
@@ -8,9 +11,12 @@ package main.java.SQLProductRetrievalAPIs;
  */
 public class TestingSQLAPI {
 	
-	public static void main (String args[]) {
+	public static void main (String args[]) throws Exception {
 		
-		
+		ArrayList<String> temp = ProductInformationSQLRetrieval.productHeaders();
+		for(int x = 0; x < temp.size(); x++) {
+			System.out.println(temp.get(x));
+		}
 		
 	}
 
